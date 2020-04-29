b30577a01f7d1b6351577939938cf84e4b2baa30
yoh2292 <guillaume.ligner@gmail.com>
2020-04-29 10:07:12 +0200

    transformer: allow command line arguments to incorporate some modules of pytorch implementation

--- transformer.py
+++ transformer.py
@@ -59,128 +59,130 @@ def attention(keys, queries, values, mask=None, temperature=None):
 class MultiHead(nn.Module):
     def __init__(self, dim_word, num_heads=8):
         super(MultiHead, self).__init__()
-        assert dim_word // num_heads * num_heads == dim_word
-        self.dim_word = dim_word
-        self.num_heads = num_heads
-        self.d_k = float(dim_word)
-        self.Q = nn.Linear(dim_word, dim_word, bias=True)
-        self.K = nn.Linear(dim_word, dim_word, bias=True)
-        self.V = nn.Linear(dim_word, dim_word, bias=True)
-        self.linear_out = nn.Linear(dim_word, dim_word, bias=True)  # bias=False
+        if not ARGS.use_pytorch_multi_head:
+            assert dim_word // num_heads * num_heads == dim_word
+            self.dim_word = dim_word
+            self.num_heads = num_heads
+            self.dim_repr = (
+                dim_word if ARGS.use_pytorch_dim_per_head else dim_word * num_heads
+            )
+            self.d_k = float(self.dim_repr)
+            self.Q = nn.Linear(dim_word, self.dim_repr, bias=True)
+            self.K = nn.Linear(dim_word, self.dim_repr, bias=True)
+            self.V = nn.Linear(dim_word, self.dim_repr, bias=True)
+            self.linear_out = nn.Linear(
+                self.dim_repr, dim_word, bias=ARGS.use_pytorch_linearout_bias
+            )  # bias=False
+        else:
+            from torch.nn.modules.activation import MultiheadAttention
 
-
-    def __init__tmp(
-        self,
-        dim_input_query,
-        dim_input_key,
-        dim_input_value,
-        dim_representation,
-        d_k=64,
-        num_heads=8,
-    ):
-        super(MultiHead, self).__init__()
-        assert dim_representation // num_heads * num_heads == dim_representation
-        from torch.nn.modules.activation import MultiheadAttention
-
-        self.multi_head = MultiheadAttention(dim_input_query, num_heads, dropout=0.0)
-
-    def forward_tmp(self, input_query, input_key, input_value, mask=None):
-        multi = self.multi_head(input_query, input_key, input_value, attn_mask=mask)[0]
-        return multi
+            self.multi_head = MultiheadAttention(dim_word, num_heads, dropout=0.0)
 
     def forward(self, input_query, input_key, input_value, mask=None):
-        assert "batch" in input_query.names and "time" in input_query.names
-        assert "batch" in input_key.names and "time" in input_key.names
-        assert "batch" in input_value.names and "time" in input_value.names
-
-        def multi_head_repr(linear_layer, input, num_heads, dim_all_heads):
-
-            multi_head_representation = linear_layer(input).refine_names(
-                ..., "dim_all_heads"
+        if ARGS.use_pytorch_multi_head:
+            return (
+                self.multi_head(
+                    input_query.align_to("time", "batch", "word_dim").rename(None),
+                    input_key.align_to("time", "batch", "word_dim").rename(None),
+                    input_value.align_to("time", "batch", "word_dim").rename(None),
+                    attn_mask=mask,
+                )[0]
+                .refine_names("time", "batch", "word_dim")
+                .align_to("batch", "time", "word_dim")
             )
-            # other possibility, dim_representation does not have to be divisible by
-            # multi_head_representation = multi_head_representation.unflatten(
-            #    "dim_times_n_head", [("head", num_heads), ("dim", dim_representation)]
-            # )
-            multi_head_representation = multi_head_representation.unflatten(
-                "dim_all_heads",
-                [("head", num_heads), ("dim", dim_all_heads // num_heads)],
+        else:
+            assert "batch" in input_query.names and "time" in input_query.names
+            assert "batch" in input_key.names and "time" in input_key.names
+            assert "batch" in input_value.names and "time" in input_value.names
+
+            def multi_head_repr(linear_layer, input, num_heads, dim_all_heads):
+
+                multi_head_representation = linear_layer(input).refine_names(
+                    ..., "dim_all_heads"
+                )
+                # other possibility, dim_representation does not have to be divisible by
+                # multi_head_representation = multi_head_representation.unflatten(
+                #    "dim_times_n_head", [("head", num_heads), ("dim", dim_representation)]
+                # )
+                multi_head_representation = multi_head_representation.unflatten(
+                    "dim_all_heads",
+                    [("head", num_heads), ("dim", dim_all_heads // num_heads)],
+                )
+                multi_head_representation = multi_head_representation.align_to(
+                    "head", "batch", "time", "dim"
+                )
+                return multi_head_representation
+
+            multi_head_q = multi_head_repr(
+                self.Q, input_query, self.num_heads, self.dim_repr
             )
-            multi_head_representation = multi_head_representation.align_to(
-                "head", "batch", "time", "dim"
+            multi_head_k = multi_head_repr(
+                self.K, input_key, self.num_heads, self.dim_repr
             )
-            return multi_head_representation
+            multi_head_v = multi_head_repr(
+                self.V, input_value, self.num_heads, self.dim_repr
+            )
 
-        multi_head_q = multi_head_repr(
-            self.Q, input_query, self.num_heads, self.dim_word
-        )
-        multi_head_k = multi_head_repr(self.K, input_key, self.num_heads, self.dim_word)
-        multi_head_v = multi_head_repr(
-            self.V, input_value, self.num_heads, self.dim_word
-        )
+            assert (
+                multi_head_k.names
+                == multi_head_q.names
+                == multi_head_v.names
+                == ("head", "batch", "time", "dim")
+            )
 
-        assert (
-            multi_head_k.names
-            == multi_head_q.names
-            == multi_head_v.names
-            == ("head", "batch", "time", "dim")
-        )
-
-        Z = attention(
-            multi_head_k,
-            multi_head_q,
-            multi_head_v,
-            mask=mask,
-            temperature=np.sqrt(self.d_k),
-        )
-        Z = Z.align_to("batch", "time", "head", "dim").flatten(
-            ["head", "dim"], "dim_all_heads"
-        )
-        Z = self.linear_out(Z).refine_names("batch", "time", "word_dim")
-        return Z
+            Z = attention(
+                multi_head_k,
+                multi_head_q,
+                multi_head_v,
+                mask=mask,
+                temperature=np.sqrt(self.d_k),
+            )
+            Z = Z.align_to("batch", "time", "head", "dim").flatten(
+                ["head", "dim"], "dim_all_heads"
+            )
+            Z = self.linear_out(Z).refine_names("batch", "time", "word_dim")
+            return Z
 
 
 class NormalizationLayer(nn.Module):
     def __init__(self, dim_word):
         super(NormalizationLayer, self).__init__()
-        self.alpha = torch.nn.Parameter(
-            data=torch.rand(1, dim_word).refine_names("time", "word_dim"),
-            requires_grad=True,
-        )
-        # they perform special init with ones and zeros for w and b
-        torch.nn.init.xavier_normal_(self.alpha)
-        self.beta = torch.nn.Parameter(
-            data=torch.rand(1, dim_word).refine_names("time", "word_dim"),
-            requires_grad=True,
-        )
-        torch.nn.init.xavier_normal_(self.beta)
+        if ARGS.use_pytorch_norm_layer:
+            from torch.nn.modules.normalization import LayerNorm
 
-    def __init__tmp(self, dim_word):
-        from torch.nn.modules.normalization import LayerNorm
-        self.layer_norm = LayerNorm(dim_word)
-
-
-    def forward_tmp(self, Z):
-        norm = self.layer_norm(Z.rename(None)).refine_names(*Z.names)
-        return norm
-
+            self.layer_norm = LayerNorm(dim_word)
+        else:
+            self.alpha = torch.nn.Parameter(
+                data=torch.rand(1, dim_word).refine_names("time", "word_dim"),
+                requires_grad=True,
+            )
+            # they perform special init with ones and zeros for w and b
+            torch.nn.init.xavier_normal_(self.alpha)
+            self.beta = torch.nn.Parameter(
+                data=torch.rand(1, dim_word).refine_names("time", "word_dim"),
+                requires_grad=True,
+            )
+            torch.nn.init.xavier_normal_(self.beta)
 
     def forward(self, Z):
-        mean = Z.mean("word_dim")
-        std = Z.std("word_dim")
-        mean = (
-            mean.rename(None)
-            .reshape(mean.shape + (1,))
-            .refine_names(*mean.names, "word_dim")
-        )
-        std = (
-            std.rename(None)
-            .reshape(std.shape + (1,))
-            .refine_names(*std.names, "word_dim")
-        )
-        assert std.names == mean.names == Z.names == ("batch", "time", "word_dim")
-        Z = (Z - mean) / std
-        return Z * self.alpha + self.beta
+        if ARGS.use_pytorch_norm_layer:
+            return self.layer_norm(Z.rename(None)).refine_names(*Z.names)
+        else:
+            mean = Z.mean("word_dim")
+            std = Z.std("word_dim")
+            mean = (
+                mean.rename(None)
+                .reshape(mean.shape + (1,))
+                .refine_names(*mean.names, "word_dim")
+            )
+            std = (
+                std.rename(None)
+                .reshape(std.shape + (1,))
+                .refine_names(*std.names, "word_dim")
+            )
+            assert std.names == mean.names == Z.names == ("batch", "time", "word_dim")
+            Z = (Z - mean) / std
+            return Z * self.alpha + self.beta
 
 
 class FeedForward(nn.Module):
@@ -203,12 +205,6 @@ class Encoder(nn.Module):
 
     # input is either original input or Z from previous encoder
     def forward(self, input):
-        # align time batch dim first
-        # Z = self.multi_head(
-        #    input_query=input.align_to("time", "batch", "word_dim").rename(None),
-        #    input_key=input.align_to("time", "batch", "word_dim").rename(None),
-        #    input_value=input.align_to("time", "batch", "word_dim").rename(None)
-        # ).refine_names("time", "batch", "word_dim").align_to("batch", "time", "word_dim")
         Z = self.multi_head(input_query=input, input_key=input, input_value=input)
         Z = self.norm_layer((Z + input.align_as(Z)))
         assert Z.names == ("batch", "time", "word_dim")
@@ -229,10 +225,7 @@ class Decoder(nn.Module):
 
     def forward(self, input_decoder, input_seq_encodings, mask=None):
         z_self_att = self.multi_head_self_att(
-            input_decoder.align_to("batch", "time", "word_dim"),
-            input_decoder.align_to("batch", "time", "word_dim"),
-            input_decoder.align_to("batch", "time", "word_dim"),
-            mask=mask,
+            input_decoder, input_decoder, input_decoder, mask=mask
         )
 
         # z_self_att = self.multi_head_self_att(
@@ -278,7 +271,8 @@ class Transformer(nn.Module):
             ]
         )
         # layer norm after encoders comes from Pytorch implemetation
-        self.layer_norm_encoder = NormalizationLayer(dim_word)
+        if ARGS.use_pytorch_norm_after_encoders_decoders:
+            self.layer_norm_encoder = NormalizationLayer(dim_word)
         self.decoders = nn.ModuleList(
             [
                 Decoder(dim_word, num_heads=num_heads, dim_feedforward=dim_feedforward)
@@ -286,17 +280,19 @@ class Transformer(nn.Module):
             ]
         )
         # layer norm after decoders comes from Pytorch implemetation
-        self.layer_norm_decoder = NormalizationLayer(dim_word)
+        if ARGS.use_pytorch_norm_after_encoders_decoders:
+            self.layer_norm_decoder = NormalizationLayer(dim_word)
         self.final_linear = nn.Linear(dim_word, decoder_vocabulary_size, bias=True)
         self.optimizer = torch.optim.Adam(self.parameters(), lr=ARGS.lr)
 
         # pytorch transformer implem part
-            # put there optimizer parameters already registered
+        # put there optimizer parameters already registered
         self.trans_final_linear = nn.Linear(
             dim_word, decoder_vocabulary_size, bias=True
         )
         # self.transformer = nn.Transformer(
-        from torch.nn.modules import transformer as PytorchTransformer
+        from torch.nn.modules.transformer import Transformer as PytorchTransformer
+        #from pytorch.torch.nn.modules.transformer import Transformer as PytorchTransformer
         self.transformer = PytorchTransformer(
             d_model=dim_word,
             nhead=num_heads,
@@ -316,17 +312,24 @@ class Transformer(nn.Module):
 
         self.criterion = nn.NLLLoss(reduction="mean")
 
-    def forward(self, input_encoder, input_decoder, mask_decoder=None, pytorch_transformer=True):
+    def forward(
+        self, input_encoder, input_decoder, pytorch_transformer=True, mask_decoder=None
+    ):
         if not pytorch_transformer:
             z_enc = input_encoder
             for encoder in self.encoders:
                 z_enc = encoder(z_enc)
+            # pytorch implem adds norm layer after encoders
+            if ARGS.use_pytorch_norm_after_encoders_decoders:
+                z_enc = self.layer_norm_encoder(z_enc)
             output_encoder = z_enc
 
             z_dec = input_decoder
             for decoder in self.decoders:
                 z_dec = decoder(z_dec, output_encoder, mask=mask_decoder)
-
+            # pytorch implem adds norm layer after decoders
+            if ARGS.use_pytorch_norm_after_encoders_decoders:
+                z_dec = self.layer_norm_decoder(z_dec)
             return torch.nn.functional.log_softmax(
                 self.final_linear(z_dec).refine_names(..., "dec_vocabulary"),
                 "dec_vocabulary",
@@ -340,22 +343,34 @@ class Transformer(nn.Module):
             )
             z_dec = (
                 self.transformer(input_encoder, input_decoder, tgt_mask=mask_decoder)
-                    .refine_names("time", "batch", "word_dim")
-                    .align_to("batch", "time", "word_dim")
+                .refine_names("time", "batch", "word_dim")
+                .align_to("batch", "time", "word_dim")
             )
             final_linear = self.trans_final_linear(z_dec).refine_names(
                 ..., "dec_vocabulary"
             )
             return torch.nn.functional.log_softmax(final_linear, "dec_vocabulary")
 
-    def train_on_batch(self, input_encoder, input_decoder, target, mask_decoder=None, pytorch_transformer=True):
+    def train_on_batch(
+        self,
+        input_encoder,
+        input_decoder,
+        target,
+        mask_decoder=None,
+        pytorch_transformer=True,
+    ):
         target = target.align_to("batch", "time", "dec_vocabulary")
         target_idx = target.rename(None).argmax(2).refine_names("batch", "time")
         # change it later
         optimizer = self.optimizer_pytorch if pytorch_transformer else self.optimizer
         # self.optimizer.zero_grad()
         optimizer.zero_grad()
-        prediction = self(input_encoder, input_decoder, mask_decoder=mask_decoder)
+        prediction = self(
+            input_encoder,
+            input_decoder,
+            pytorch_transformer=pytorch_transformer,
+            mask_decoder=mask_decoder,
+        )
         loss_on_batch = self.criterion(
             prediction.flatten(["batch", "time"], "batch_time").rename(None),
             target_idx.flatten(["batch", "time"], "batch_time").rename(None),
@@ -371,6 +386,42 @@ def handle_arguments():
     ARG_PARSER.add_argument(
         "--quick-debug", default=True, type=lambda x: str(x).lower() == "true", help=""
     )
+    ARG_PARSER.add_argument(
+        "--use-mask",
+        default=True,
+        type=lambda x: str(x).lower() == "true",
+        help="",
+    )
+    ARG_PARSER.add_argument(
+        "--use-pytorch-multi-head",
+        default=True,
+        type=lambda x: str(x).lower() == "true",
+        help="",
+    )
+    ARG_PARSER.add_argument(
+        "--use-pytorch-dim-per-head",
+        default=True,
+        type=lambda x: str(x).lower() == "true",
+        help="",
+    )
+    ARG_PARSER.add_argument(
+        "--use-pytorch-norm-layer",
+        default=True,
+        type=lambda x: str(x).lower() == "true",
+        help="",
+    )
+    ARG_PARSER.add_argument(
+        "--use-pytorch-linearout-bias",
+        default=True,
+        type=lambda x: str(x).lower() == "true",
+        help="",
+    )
+    ARG_PARSER.add_argument(
+        "--use-pytorch-norm-after-encoders-decoders",
+        default=True,
+        type=lambda x: str(x).lower() == "true",
+        help="",
+    )
 
     # Transformer architecture arguments
     ARG_PARSER.add_argument("--num-heads", default=2, type=int, help="")
@@ -381,9 +432,6 @@ def handle_arguments():
     ARG_PARSER.add_argument("--lr", default=0.01, type=float, help="")
     ARG_PARSER.add_argument("--batch-size", default=128, type=int, help="")
     ARG_PARSER.add_argument("--epochs", default=2000, type=int, help="")
-    ARG_PARSER.add_argument(
-        "--use-mask", default=True, type=lambda x: str(x).lower() == "true", help=""
-    )
 
     return ARG_PARSER.parse_args()
 
@@ -399,21 +447,14 @@ def generate_batches(encoder_inputs, decoder_inputs, targets, batch_size):
     np.random.shuffle(samples_idxs)
     nb_batch = encoder_inputs.size("batch") // batch_size
     # so can keep on calling generator after inner loop has expired all batches
-    while True:
-        for batch_idx in range(nb_batch):
-            batch_idxs = samples_idxs[
-                batch_idx * batch_size : (batch_idx + 1) * batch_size
-            ]
-            batch = (
-                encoder_inputs.rename(None)[batch_idxs].refine_names(
-                    *encoder_inputs.names
-                ),
-                decoder_inputs.rename(None)[batch_idxs].refine_names(
-                    *decoder_inputs.names
-                ),
-                targets.rename(None)[batch_idxs].refine_names(*targets.names),
-            )
-            yield batch
+    for batch_idx in range(nb_batch):
+        batch_idxs = samples_idxs[batch_idx * batch_size : (batch_idx + 1) * batch_size]
+        batch = (
+            encoder_inputs.rename(None)[batch_idxs].refine_names(*encoder_inputs.names),
+            decoder_inputs.rename(None)[batch_idxs].refine_names(*decoder_inputs.names),
+            targets.rename(None)[batch_idxs].refine_names(*targets.names),
+        )
+        yield batch
 
 
 # this code is for toy data generation, will be discarded later
@@ -482,13 +523,6 @@ if __name__ == "__main__":
             data_processor.target_tr,
             ARGS.batch_size,
         ):
-            batch_loss = transformer.train_on_batch(
-                batch_encoder_inputs,
-                batch_decoder_inputs,
-                batch_targets,
-                mask_decoder=mask_decoder,
-            )
-            print(f"personal implem loss on batch: {batch_loss} epoch: {epoch}")
             msg = ""
             # train alternatively pytorch implementation and our implementation
             for use_pytorch_transformer in True, False:
@@ -496,6 +530,7 @@ if __name__ == "__main__":
                     batch_encoder_inputs,
                     batch_decoder_inputs,
                     batch_targets,
+                    mask_decoder=mask_decoder,
                     pytorch_transformer=use_pytorch_transformer,
                 )
                 if use_pytorch_transformer:

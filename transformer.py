1d42238e4b2312a08e40f1bd808de050c41bde1c
yoh2292 <guillaume.ligner@gmail.com>
2020-04-30 11:57:12 +0200

    transformer: reordering classes, functions in module

--- transformer.py
+++ transformer.py
@@ -3,184 +3,6 @@ import torch.nn as nn
 import torch
 
 
-def decoder_triangular_training_mask(nb_timesteps):
-    mask = torch.triu(torch.ones(nb_timesteps, nb_timesteps), diagonal=1).type(
-        torch.bool
-    )
-    return mask
-
-
-def positional_encoding(position, encoding_dim):
-    dimensions = np.arange(encoding_dim)
-    dimensions_i = dimensions // 2
-    dimensions_are_even = dimensions % 2 == 0
-    positional_encoding = position / (10000 ** (2 * dimensions_i / encoding_dim))
-    positional_encoding[dimensions_are_even] = np.sin(
-        positional_encoding[dimensions_are_even]
-    )
-    positional_encoding[~dimensions_are_even] = np.cos(
-        positional_encoding[~dimensions_are_even]
-    )
-    return positional_encoding
-
-
-def attention(keys, queries, values, mask=None, temperature=None):
-    assert keys.names == values.names == queries.names
-    assert "time" in keys.names and "dim" in keys.names
-    assert keys.size("time") == values.size("time")
-    other_names = tuple(name for name in keys.names if name not in ("time", "dim"))
-
-    Z = torch.matmul(
-        queries,
-        keys.rename(time="time_keys").align_to(*other_names, "dim", "time_keys"),
-    )
-    if temperature:
-        Z /= temperature
-    assert Z.names == other_names + ("time", "time_keys")
-
-    # if mask provided then mask some positions from softmax computation
-    if mask is not None:
-        mask_infs = torch.zeros(mask.shape)
-        mask_infs[mask] -= torch.Tensor([float("Inf")])
-        Z += mask_infs
-    Z = torch.nn.functional.softmax(Z, "time_keys")
-
-    Z = torch.matmul(
-        Z, values.rename(time="time_keys").align_to(*other_names, "time_keys", "dim")
-    )
-    return Z
-
-
-class MultiHead(nn.Module):
-    def __init__(
-        self,
-        dim_word,
-        num_heads=8,
-        dim_key_query_per_head=None,
-        dim_value_per_head=None,
-    ):
-        super(MultiHead, self).__init__()
-        self.dim_word = dim_word
-        self.num_heads = num_heads
-        self.dim_key_query_all_heads, self.dim_value_all_heads = (
-            self.dim_word,
-            self.dim_word,
-        )
-        if dim_key_query_per_head is not None:
-            self.dim_key_query_all_heads = dim_key_query_per_head * num_heads
-        if dim_value_per_head is not None:
-            self.dim_value_all_heads = dim_value_per_head * num_heads
-
-        assert (
-            self.dim_key_query_all_heads // num_heads * num_heads
-            == self.dim_key_query_all_heads
-        )
-        assert (
-            self.dim_value_all_heads // num_heads * num_heads
-            == self.dim_value_all_heads
-        )
-        # the greater the number of dimensions involved in the dot products
-        # before softmax the more we scale to flatten the probabilities
-        self.d_k = float(self.dim_key_query_all_heads)
-        self.Q = nn.Linear(dim_word, self.dim_key_query_all_heads, bias=True)
-        self.K = nn.Linear(dim_word, self.dim_key_query_all_heads, bias=True)
-        self.V = nn.Linear(dim_word, self.dim_value_all_heads, bias=True)
-        self.linear_out = nn.Linear(self.dim_value_all_heads, dim_word, bias=True)
-
-    def forward(self, input_query, input_key, input_value, mask=None):
-        assert "batch" in input_query.names and "time" in input_query.names
-        assert "batch" in input_key.names and "time" in input_key.names
-        assert "batch" in input_value.names and "time" in input_value.names
-
-        def multi_head_repr(linear_layer, input, num_heads, dim_all_heads):
-
-            multi_head_representation = linear_layer(input).refine_names(
-                ..., "dim_all_heads"
-            )
-            multi_head_representation = multi_head_representation.unflatten(
-                "dim_all_heads",
-                [("head", num_heads), ("dim", dim_all_heads // num_heads)],
-            )
-            multi_head_representation = multi_head_representation.align_to(
-                "head", "batch", "time", "dim"
-            )
-            return multi_head_representation
-
-        multi_head_q = multi_head_repr(
-            self.Q, input_query, self.num_heads, self.dim_key_query_all_heads
-        )
-        multi_head_k = multi_head_repr(
-            self.K, input_key, self.num_heads, self.dim_key_query_all_heads
-        )
-        multi_head_v = multi_head_repr(
-            self.V, input_value, self.num_heads, self.dim_value_all_heads
-        )
-
-        assert (
-            multi_head_k.names
-            == multi_head_q.names
-            == multi_head_v.names
-            == ("head", "batch", "time", "dim")
-        )
-
-        Z = attention(
-            multi_head_k,
-            multi_head_q,
-            multi_head_v,
-            mask=mask,
-            temperature=np.sqrt(self.d_k),
-        )
-        Z = Z.align_to("batch", "time", "head", "dim").flatten(
-            ["head", "dim"], "dim_all_heads"
-        )
-        Z = self.linear_out(Z).refine_names("batch", "time", "word_dim")
-        return Z
-
-
-class NormalizationLayer(nn.Module):
-    def __init__(self, dim_word):
-        super(NormalizationLayer, self).__init__()
-        self.alpha = torch.nn.Parameter(
-            data=torch.rand(1, dim_word).refine_names("time", "word_dim"),
-            requires_grad=True,
-        )
-        # pytorch trick, special init with ones and zeros for alpha and beta
-        torch.nn.init.ones_(self.alpha)
-
-        self.beta = torch.nn.Parameter(
-            data=torch.rand(1, dim_word).refine_names("time", "word_dim"),
-            requires_grad=True,
-        )
-        torch.nn.init.zeros_(self.beta)
-
-    def forward(self, Z):
-        mean = Z.mean("word_dim")
-        std = Z.std("word_dim")
-        mean = (
-            mean.rename(None)
-            .reshape(mean.shape + (1,))
-            .refine_names(*mean.names, "word_dim")
-        )
-        std = (
-            std.rename(None)
-            .reshape(std.shape + (1,))
-            .refine_names(*std.names, "word_dim")
-        )
-        assert std.names == mean.names == Z.names == ("batch", "time", "word_dim")
-        Z = (Z - mean) / std
-        return Z * self.alpha + self.beta
-
-
-class FeedForward(nn.Module):
-    def __init__(self, word_dim, dim_feedforward=2048):
-        super(FeedForward, self).__init__()
-        self.linear1 = nn.Linear(word_dim, dim_feedforward, bias=True)
-        self.linear2 = nn.Linear(dim_feedforward, word_dim, bias=True)
-
-    def forward(self, input):
-        return self.linear2(torch.nn.functional.relu(self.linear1(input)))
-
-
 class Transformer(nn.Module):
     def __init__(
         self,
@@ -271,3 +93,181 @@ class Encoder(nn.Module):
         Z_forwarded = self.ffnn(Z).refine_names(*Z.names)
         Z_final = self.norm_layer2((Z + Z_forwarded))
         return Z_final
+
+
+class FeedForward(nn.Module):
+    def __init__(self, word_dim, dim_feedforward=2048):
+        super(FeedForward, self).__init__()
+        self.linear1 = nn.Linear(word_dim, dim_feedforward, bias=True)
+        self.linear2 = nn.Linear(dim_feedforward, word_dim, bias=True)
+
+    def forward(self, input):
+        return self.linear2(torch.nn.functional.relu(self.linear1(input)))
+
+
+class NormalizationLayer(nn.Module):
+    def __init__(self, dim_word):
+        super(NormalizationLayer, self).__init__()
+        self.alpha = torch.nn.Parameter(
+            data=torch.rand(1, dim_word).refine_names("time", "word_dim"),
+            requires_grad=True,
+        )
+        # pytorch trick, special init with ones and zeros for alpha and beta
+        torch.nn.init.ones_(self.alpha)
+
+        self.beta = torch.nn.Parameter(
+            data=torch.rand(1, dim_word).refine_names("time", "word_dim"),
+            requires_grad=True,
+        )
+        torch.nn.init.zeros_(self.beta)
+
+    def forward(self, Z):
+        mean = Z.mean("word_dim")
+        std = Z.std("word_dim")
+        mean = (
+            mean.rename(None)
+            .reshape(mean.shape + (1,))
+            .refine_names(*mean.names, "word_dim")
+        )
+        std = (
+            std.rename(None)
+            .reshape(std.shape + (1,))
+            .refine_names(*std.names, "word_dim")
+        )
+        assert std.names == mean.names == Z.names == ("batch", "time", "word_dim")
+        Z = (Z - mean) / std
+        return Z * self.alpha + self.beta
+
+
+class MultiHead(nn.Module):
+    def __init__(
+        self,
+        dim_word,
+        num_heads=8,
+        dim_key_query_per_head=None,
+        dim_value_per_head=None,
+    ):
+        super(MultiHead, self).__init__()
+        self.dim_word = dim_word
+        self.num_heads = num_heads
+        self.dim_key_query_all_heads, self.dim_value_all_heads = (
+            self.dim_word,
+            self.dim_word,
+        )
+        if dim_key_query_per_head is not None:
+            self.dim_key_query_all_heads = dim_key_query_per_head * num_heads
+        if dim_value_per_head is not None:
+            self.dim_value_all_heads = dim_value_per_head * num_heads
+
+        assert (
+            self.dim_key_query_all_heads // num_heads * num_heads
+            == self.dim_key_query_all_heads
+        )
+        assert (
+            self.dim_value_all_heads // num_heads * num_heads
+            == self.dim_value_all_heads
+        )
+        # the greater the number of dimensions involved in the dot products
+        # before softmax the more we scale to flatten the probabilities
+        self.d_k = float(self.dim_key_query_all_heads)
+        self.Q = nn.Linear(dim_word, self.dim_key_query_all_heads, bias=True)
+        self.K = nn.Linear(dim_word, self.dim_key_query_all_heads, bias=True)
+        self.V = nn.Linear(dim_word, self.dim_value_all_heads, bias=True)
+        self.linear_out = nn.Linear(self.dim_value_all_heads, dim_word, bias=True)
+
+    def forward(self, input_query, input_key, input_value, mask=None):
+        assert "batch" in input_query.names and "time" in input_query.names
+        assert "batch" in input_key.names and "time" in input_key.names
+        assert "batch" in input_value.names and "time" in input_value.names
+
+        def multi_head_repr(linear_layer, input, num_heads, dim_all_heads):
+
+            multi_head_representation = linear_layer(input).refine_names(
+                ..., "dim_all_heads"
+            )
+            multi_head_representation = multi_head_representation.unflatten(
+                "dim_all_heads",
+                [("head", num_heads), ("dim", dim_all_heads // num_heads)],
+            )
+            multi_head_representation = multi_head_representation.align_to(
+                "head", "batch", "time", "dim"
+            )
+            return multi_head_representation
+
+        multi_head_q = multi_head_repr(
+            self.Q, input_query, self.num_heads, self.dim_key_query_all_heads
+        )
+        multi_head_k = multi_head_repr(
+            self.K, input_key, self.num_heads, self.dim_key_query_all_heads
+        )
+        multi_head_v = multi_head_repr(
+            self.V, input_value, self.num_heads, self.dim_value_all_heads
+        )
+
+        assert (
+            multi_head_k.names
+            == multi_head_q.names
+            == multi_head_v.names
+            == ("head", "batch", "time", "dim")
+        )
+
+        Z = attention(
+            multi_head_k,
+            multi_head_q,
+            multi_head_v,
+            mask=mask,
+            temperature=np.sqrt(self.d_k),
+        )
+        Z = Z.align_to("batch", "time", "head", "dim").flatten(
+            ["head", "dim"], "dim_all_heads"
+        )
+        Z = self.linear_out(Z).refine_names("batch", "time", "word_dim")
+        return Z
+
+
+def attention(keys, queries, values, mask=None, temperature=None):
+    assert keys.names == values.names == queries.names
+    assert "time" in keys.names and "dim" in keys.names
+    assert keys.size("time") == values.size("time")
+    other_names = tuple(name for name in keys.names if name not in ("time", "dim"))
+
+    Z = torch.matmul(
+        queries,
+        keys.rename(time="time_keys").align_to(*other_names, "dim", "time_keys"),
+    )
+    if temperature:
+        Z /= temperature
+    assert Z.names == other_names + ("time", "time_keys")
+
+    # if mask provided then mask some positions from softmax computation
+    if mask is not None:
+        mask_infs = torch.zeros(mask.shape)
+        mask_infs[mask] -= torch.Tensor([float("Inf")])
+        Z += mask_infs
+    Z = torch.nn.functional.softmax(Z, "time_keys")
+
+    Z = torch.matmul(
+        Z, values.rename(time="time_keys").align_to(*other_names, "time_keys", "dim")
+    )
+    return Z
+
+
+def positional_encoding(position, encoding_dim):
+    dimensions = np.arange(encoding_dim)
+    dimensions_i = dimensions // 2
+    dimensions_are_even = dimensions % 2 == 0
+    positional_encoding = position / (10000 ** (2 * dimensions_i / encoding_dim))
+    positional_encoding[dimensions_are_even] = np.sin(
+        positional_encoding[dimensions_are_even]
+    )
+    positional_encoding[~dimensions_are_even] = np.cos(
+        positional_encoding[~dimensions_are_even]
+    )
+    return positional_encoding
+
+
+def decoder_triangular_training_mask(nb_timesteps):
+    mask = torch.triu(torch.ones(nb_timesteps, nb_timesteps), diagonal=1).type(
+        torch.bool
+    )
+    return mask

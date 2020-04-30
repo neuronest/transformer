9f581840bd4d3f071b95ceb4c7b2c345a4378c9d
yoh2292 <guillaume.ligner@gmail.com>
2020-04-30 11:52:42 +0200

    transformer: removed everything that is related tp the transformer itself

--- transformer.py
+++ transformer.py
@@ -1,10 +1,6 @@
 import numpy as np
 import torch.nn as nn
 import torch
-import argparse
-
-ARG_PARSER = argparse.ArgumentParser()
-ARGS = None
 
 
 def decoder_triangular_training_mask(nb_timesteps):
@@ -185,22 +181,49 @@ class FeedForward(nn.Module):
         return self.linear2(torch.nn.functional.relu(self.linear1(input)))
 
 
-class Encoder(nn.Module):
-    def __init__(self, dim_word, num_heads=8, dim_feedforward=2048):
-        super(Encoder, self).__init__()
-        self.multi_head = MultiHead(dim_word, num_heads=num_heads)
-        self.norm_layer = NormalizationLayer(dim_word)
-        self.ffnn = FeedForward(dim_word, dim_feedforward=dim_feedforward)
-        self.norm_layer2 = NormalizationLayer(dim_word)
+class Transformer(nn.Module):
+    def __init__(
+        self,
+        dim_word,
+        decoder_vocabulary_size,
+        num_heads=8,
+        num_encoders=6,
+        num_decoders=6,
+        dim_feedforward=2048,
+    ):
+        super(Transformer, self).__init__()
+        self.encoders = nn.ModuleList(
+            [
+                Encoder(dim_word, num_heads=num_heads, dim_feedforward=dim_feedforward)
+                for _ in range(num_encoders)
+            ]
+        )
+        # layer norm after encoders comes from Pytorch implemetation
+        self.layer_norm_encoder = NormalizationLayer(dim_word)
+        self.decoders = nn.ModuleList(
+            [
+                Decoder(dim_word, num_heads=num_heads, dim_feedforward=dim_feedforward)
+                for _ in range(num_decoders)
+            ]
+        )
+        # layer norm after decoders comes from Pytorch implemetation
+        self.layer_norm_decoder = NormalizationLayer(dim_word)
+        self.final_linear = nn.Linear(dim_word, decoder_vocabulary_size, bias=True)
 
-    # input is either original input or Z from previous encoder
-    def forward(self, input):
-        Z = self.multi_head(input_query=input, input_key=input, input_value=input)
-        Z = self.norm_layer((Z + input.align_as(Z)))
-        assert Z.names == ("batch", "time", "word_dim")
-        Z_forwarded = self.ffnn(Z).refine_names(*Z.names)
-        Z_final = self.norm_layer2((Z + Z_forwarded))
-        return Z_final
+    def forward(self, input_encoder, input_decoder, mask_decoder=None):
+        z_enc = input_encoder
+        for encoder in self.encoders:
+            z_enc = encoder(z_enc)
+        # pytorch implem adds norm layer after encoders
+        z_enc = self.layer_norm_encoder(z_enc)
+        output_encoder = z_enc
+
+        z_dec = input_decoder
+        for decoder in self.decoders:
+            z_dec = decoder(z_dec, output_encoder, mask=mask_decoder)
+        # pytorch implem adds norm layer after decoders
+        z_dec = self.layer_norm_decoder(z_dec)
+        return z_dec
 
 
 class Decoder(nn.Module):
@@ -232,397 +255,19 @@ class Decoder(nn.Module):
         return Z_final
 
 
-class Transformer(nn.Module):
-    def __init__(
-        self,
-        dim_word,
-        decoder_vocabulary_size,
-        num_heads=8,
-        num_encoders=6,
-        num_decoders=6,
-        dim_feedforward=2048,
-    ):
-        super(Transformer, self).__init__()
-        self.encoders = nn.ModuleList(
-            [
-                Encoder(dim_word, num_heads=num_heads, dim_feedforward=dim_feedforward)
-                for _ in range(num_encoders)
-            ]
-        )
-        # layer norm after encoders comes from Pytorch implemetation
-        self.layer_norm_encoder = NormalizationLayer(dim_word)
-        self.decoders = nn.ModuleList(
-            [
-                Decoder(dim_word, num_heads=num_heads, dim_feedforward=dim_feedforward)
-                for _ in range(num_decoders)
-            ]
-        )
-        # layer norm after decoders comes from Pytorch implemetation
-        self.layer_norm_decoder = NormalizationLayer(dim_word)
-        self.final_linear = nn.Linear(dim_word, decoder_vocabulary_size, bias=True)
-        self.optimizer = torch.optim.Adam(self.parameters(), lr=ARGS.lr)
+class Encoder(nn.Module):
+    def __init__(self, dim_word, num_heads=8, dim_feedforward=2048):
+        super(Encoder, self).__init__()
+        self.multi_head = MultiHead(dim_word, num_heads=num_heads)
+        self.norm_layer = NormalizationLayer(dim_word)
+        self.ffnn = FeedForward(dim_word, dim_feedforward=dim_feedforward)
+        self.norm_layer2 = NormalizationLayer(dim_word)
 
-        # pytorch transformer implem part
-        # put there optimizer parameters already registered
-        self.trans_final_linear = nn.Linear(
-            dim_word, decoder_vocabulary_size, bias=True
-        )
-        # self.transformer = nn.Transformer(
-        from torch.nn.modules.transformer import Transformer as PytorchTransformer
-
-        # from pytorch.torch.nn.modules.transformer import Transformer as PytorchTransformer
-        self.transformer = PytorchTransformer(
-            d_model=dim_word,
-            nhead=num_heads,
-            num_encoder_layers=num_encoders,
-            num_decoder_layers=num_decoders,
-            dim_feedforward=dim_feedforward,
-            dropout=0.0,
-            activation="relu",
-            custom_encoder=None,
-            custom_decoder=None,
-        )
-        self.optimizer_pytorch = torch.optim.Adam(
-            list(self.transformer.parameters())
-            + list(self.trans_final_linear.parameters()),
-            lr=ARGS.lr,
-        )
-
-        self.criterion = nn.NLLLoss(reduction="mean")
-
-    def forward(
-        self, input_encoder, input_decoder, pytorch_transformer=True, mask_decoder=None
-    ):
-        if not pytorch_transformer:
-            z_enc = input_encoder
-            for encoder in self.encoders:
-                z_enc = encoder(z_enc)
-            # pytorch implem adds norm layer after encoders
-            z_enc = self.layer_norm_encoder(z_enc)
-            output_encoder = z_enc
-
-            z_dec = input_decoder
-            for decoder in self.decoders:
-                z_dec = decoder(z_dec, output_encoder, mask=mask_decoder)
-            # pytorch implem adds norm layer after decoders
-            z_dec = self.layer_norm_decoder(z_dec)
-            return torch.nn.functional.log_softmax(
-                self.final_linear(z_dec).refine_names(..., "dec_vocabulary"),
-                "dec_vocabulary",
-            )
-        else:
-            input_encoder = input_encoder.align_to("time", "batch", "word_dim").rename(
-                None
-            )
-            input_decoder = input_decoder.align_to("time", "batch", "word_dim").rename(
-                None
-            )
-            z_dec = (
-                self.transformer(input_encoder, input_decoder, tgt_mask=mask_decoder)
-                .refine_names("time", "batch", "word_dim")
-                .align_to("batch", "time", "word_dim")
-            )
-            final_linear = self.trans_final_linear(z_dec).refine_names(
-                ..., "dec_vocabulary"
-            )
-            return torch.nn.functional.log_softmax(final_linear, "dec_vocabulary")
-
-    def train_on_batch(
-        self,
-        input_encoder,
-        input_decoder,
-        target,
-        mask_decoder=None,
-        pytorch_transformer=True,
-    ):
-        # change it later
-        optimizer = self.optimizer_pytorch if pytorch_transformer else self.optimizer
-        # self.optimizer.zero_grad()
-        optimizer.zero_grad()
-        prediction = self(
-            input_encoder,
-            input_decoder,
-            pytorch_transformer=pytorch_transformer,
-            mask_decoder=mask_decoder,
-        )
-        loss_on_batch = self._batched_ce_loss(prediction, target)
-        loss_on_batch.backward()
-        # self.optimizer.step()
-        optimizer.step()
-        return loss_on_batch
-
-    def _batched_ce_loss(self, prediction, target, reduction="mean"):
-        target = target.align_to("batch", "time", "dec_vocabulary")
-        target_idx = target.rename(None).argmax(2).refine_names("batch", "time")
-        batched_ce_loss = nn.NLLLoss(reduction=reduction)(
-            prediction.flatten(["batch", "time"], "batch_time").rename(None),
-            target_idx.flatten(["batch", "time"], "batch_time").rename(None),
-        )
-        return batched_ce_loss
-
-    def validate(self, validation_data, pytorch_transformer, mask_decoder=None):
-        input_val, target_val = validation_data
-        prediction_val = self(
-            *input_val,
-            pytorch_transformer=pytorch_transformer,
-            mask_decoder=mask_decoder,
-        )
-        loss_on_val = self._batched_ce_loss(prediction_val, target_val)
-        return loss_on_val
-
-    def infer(
-        self,
-        math_expression,
-        math_expression_target,
-        char_to_idx,
-        idx_to_char,
-        max_encode_len,
-        max_decode_len,
-        pytorch_transformer,
-        voc_size,
-        GO="=",
-        EOS="\n",
-        enc_input=None,
-        dec_input=None,
-    ):
-        target_idxs = [char_to_idx[c] for c in math_expression_target]
-
-        dec_input_idxs = dec_input.rename(None).argmax(2)[0]
-
-        # +1 for GO term
-        position_encodings = torch.tensor(
-            [
-                positional_encoding(t, voc_size)
-                for t in range(max(max_encode_len, max_decode_len) + 1)
-            ],
-            dtype=torch.float32,
-        ).refine_names("time", "word_dim")
-
-        mask_decoder = decoder_triangular_training_mask(dec_input.size("time"))
-        teacher_forcing_preds = self(
-            enc_input,
-            dec_input,
-            pytorch_transformer=pytorch_transformer,
-            mask_decoder=mask_decoder,
-        )
-        teacher_forcing_preds_idxs = teacher_forcing_preds.rename(None).argmax(2)
-        teacher_forcing_preds_chars = [
-            idx_to_char[int(idx)] for idx in teacher_forcing_preds_idxs[0]
-        ]
-
-        encoder_input_idxs = [char_to_idx[c] for c in math_expression]
-        encoder_input = torch.zeros(1, max_encode_len, voc_size)
-        encoder_input[0, np.arange(len(encoder_input_idxs)), encoder_input_idxs] = 1
-        encoder_input = encoder_input.refine_names("batch", "time", "word_dim")
-        encoder_input += position_encodings[: encoder_input.size("time")]
-
-        decoder_input_idxs = [char_to_idx[GO]]
-        decoder_input = torch.zeros(1, len(decoder_input_idxs), voc_size)
-        decoder_input[0, np.arange(len(decoder_input_idxs)), decoder_input_idxs] = 1
-        decoder_input = decoder_input.refine_names("batch", "time", "word_dim")
-        decoder_input += position_encodings[: decoder_input.size("time")]
-
-        decoded_expression = []
-        for _ in range(max_decode_len):
-            # see that
-            mask_decoder = decoder_triangular_training_mask(decoder_input.size("time"))
-            pred_tensor = self(
-                encoder_input, decoder_input, pytorch_transformer=pytorch_transformer
-            )
-            # last timestep
-            pred_tensor = pred_tensor.rename(None)[0, -1, :]
-            pred_idx = int(pred_tensor.argmax())
-            if pred_idx == char_to_idx[EOS]:
-                break
-            decoder_input_idxs.append(int(pred_idx))
-            decoder_input = torch.zeros(1, len(decoder_input_idxs), voc_size)
-            decoder_input[0, np.arange(len(decoder_input_idxs)), decoder_input_idxs] = 1
-            decoder_input = decoder_input.refine_names("batch", "time", "word_dim")
-            decoder_input += position_encodings[: decoder_input.size("time")]
-
-            pred_char = idx_to_char[pred_idx]
-            decoded_expression.append(pred_char)
-        return "".join(decoded_expression)
-
-
-def handle_arguments():
-    # Debug arguments
-    ARG_PARSER.add_argument(
-        "--quick-debug", default=True, type=lambda x: str(x).lower() == "true", help=""
-    )
-    # Transformer architecture arguments
-    ARG_PARSER.add_argument("--num-heads", default=2, type=int, help="")
-    ARG_PARSER.add_argument("--num-encoders", default=1, type=int, help="")
-    ARG_PARSER.add_argument("--num-decoders", default=1, type=int, help="")
-
-    # Training arguments
-    ARG_PARSER.add_argument("--lr", default=0.01, type=float, help="")
-    ARG_PARSER.add_argument("--batch-size", default=128, type=int, help="")
-    ARG_PARSER.add_argument("--epochs", default=2000, type=int, help="")
-    ARG_PARSER.add_argument(
-        "--validate", default=True, type=lambda x: str(x).lower() == "true", help=""
-    )
-    ARG_PARSER.add_argument(
-        "--do-inference", default=True, type=lambda x: str(x).lower() == "true", help=""
-    )
-
-    return ARG_PARSER.parse_args()
-
-
-def generate_batches(encoder_inputs, decoder_inputs, targets, batch_size):
-    assert (
-        encoder_inputs.names[0]
-        == decoder_inputs.names[0]
-        == targets.names[0]
-        == "batch"
-    )
-    samples_idxs = np.arange(encoder_inputs.size("batch"))
-    np.random.shuffle(samples_idxs)
-    nb_batch = encoder_inputs.size("batch") // batch_size
-    # so can keep on calling generator after inner loop has expired all batches
-    for batch_idx in range(nb_batch):
-        batch_idxs = samples_idxs[batch_idx * batch_size : (batch_idx + 1) * batch_size]
-        batch = (
-            encoder_inputs.rename(None)[batch_idxs].refine_names(*encoder_inputs.names),
-            decoder_inputs.rename(None)[batch_idxs].refine_names(*decoder_inputs.names),
-            targets.rename(None)[batch_idxs].refine_names(*targets.names),
-        )
-        yield batch
-
-
-# this code is for toy data generation, will be discarded later
-def get_math_data(small_nb_samples=200, big_nb_samples=int(1e5), big_dataset=True):
-    import operator
-
-    # raises error if put at file top as data_processor
-    # imports transformer module function
-    # not a problem since get_math_data will be discarded eventually
-    from data_processor import DataProcessor
-
-    def math_expressions_generation(n_samples=1000, n_digits=3, invert=True):
-        X, Y = [], []
-        math_operators = {
-            "+": operator.add,
-            "-": operator.sub,
-            "*": operator.mul,
-            "/": operator.truediv,
-            "%": operator.mod,
-        }
-        for i in range(n_samples):
-            a, b = np.random.randint(1, 10 ** n_digits, size=2)
-            op = np.random.choice(list(math_operators.keys()))
-            res = math_operators[op](a, b)
-            x = "".join([str(elem) for elem in (a, op, b)])
-            if invert is True:
-                x = x[::-1]
-            y = "{:.5f}".format(res) if isinstance(res, float) else str(res)
-            X.append(x)
-            Y.append(y)
-        return X, Y
-
-    n_samples = big_nb_samples if big_dataset else small_nb_samples
-    X, y = math_expressions_generation(n_samples=n_samples, n_digits=3, invert=False)
-    data_processor = DataProcessor(X, y, with_positional_encodings=True)
-    assert data_processor.encoder_input_tr.size(
-        "word_dim"
-    ) == data_processor.decoder_input_tr.size("word_dim")
-    return data_processor
-
-
-if __name__ == "__main__":
-
-    ARGS = handle_arguments()
-    data_processor = get_math_data(big_dataset=not ARGS.quick_debug)
-    transformer = Transformer(
-        data_processor.encoder_input_tr.size("word_dim"),
-        data_processor.vocabulary_size,
-        num_heads=ARGS.num_heads,
-        num_encoders=ARGS.num_encoders,
-        num_decoders=ARGS.num_decoders,
-    )
-    mask_decoder = (
-        decoder_triangular_training_mask(data_processor.decoder_input_tr.size("time"))
-    )
-    validation_data = (
-        (
-            (data_processor.encoder_input_val, data_processor.decoder_input_val),
-            data_processor.target_val,
-        )
-        if ARGS.validate
-        else None
-    )
-    for epoch in range(ARGS.epochs):
-        for (
-            batch_encoder_inputs,
-            batch_decoder_inputs,
-            batch_targets,
-        ) in generate_batches(
-            data_processor.encoder_input_tr,
-            data_processor.decoder_input_tr,
-            data_processor.target_tr,
-            ARGS.batch_size,
-        ):
-            msg = ""
-            # train alternatively pytorch implementation and our implementation
-            for use_pytorch_transformer in True, False:
-                batch_loss = transformer.train_on_batch(
-                    batch_encoder_inputs,
-                    batch_decoder_inputs,
-                    batch_targets,
-                    mask_decoder=mask_decoder,
-                    pytorch_transformer=use_pytorch_transformer,
-                )
-                if use_pytorch_transformer:
-                    msg += f"pytorch implem loss on batch: {batch_loss}"
-                else:
-                    msg += f" personal implem loss on batch: {batch_loss}"
-            msg += f" epoch: {epoch}"
-            print(msg)
-        if validation_data is not None:
-            msg = ""
-            for use_pytorch_transformer in True, False:
-                loss_val = transformer.validate(
-                    validation_data, use_pytorch_transformer, mask_decoder=None
-                )
-                if use_pytorch_transformer:
-                    msg += f"pytorch implem val loss: {loss_val}"
-                else:
-                    msg += f" personal implem val loss: {loss_val}"
-            print(msg)
-
-        if (ARGS.do_inference and not ARGS.quick_debug) or (ARGS.quick_debug and ARGS.do_inference and batch_loss < 0.1):
-            if ARGS.quick_debug:
-                X = data_processor.X_tr
-                y = data_processor.y_tr
-                enc_input = data_processor.encoder_input_tr
-                dec_input = data_processor.decoder_input_tr
-            else:
-                X = data_processor.X_val
-                y = data_processor.y_val
-                enc_input = data_processor.encoder_input_val
-                dec_input = data_processor.decoder_input_val
-
-            for use_pytorch_transformer in True, False:
-                if use_pytorch_transformer:
-                    print(os.linesep, "Inferences for pytorch implem")
-                else:
-                    print(os.linesep, "Inferences for personal implem")
-                for i in range(10):
-                    y_pred = transformer.infer(
-                        X[i],
-                        y[i].replace(data_processor.GO, ''),
-                        data_processor.char_index,
-                        data_processor.index_char,
-                        data_processor.max_encoder_sequence_length,
-                        data_processor.max_decoder_sequence_length,
-                        use_pytorch_transformer,
-                        data_processor.vocabulary_size,
-                        GO=data_processor.GO,
-                        EOS=data_processor.EOS,
-                        enc_input=enc_input.rename(None)[i:i + 1].refine_names(*enc_input.names),
-                        dec_input=dec_input.rename(None)[i:i + 1].refine_names(*dec_input.names)
-                    )
-                    print(
-                        f"{X[i]} = {y_pred} predicted / {y[i].replace(data_processor.GO, '').replace(data_processor.EOS, '')} expected"
-                    )
-                print()
+    # input is either original input or Z from previous encoder
+    def forward(self, input):
+        Z = self.multi_head(input_query=input, input_key=input, input_value=input)
+        Z = self.norm_layer((Z + input.align_as(Z)))
+        assert Z.names == ("batch", "time", "word_dim")
+        Z_forwarded = self.ffnn(Z).refine_names(*Z.names)
+        Z_final = self.norm_layer2((Z + Z_forwarded))
+        return Z_final
